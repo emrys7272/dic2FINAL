24e3c8edf8c20ec081c4b121c8458480c4e69656
Parvin <youngwarlock1401@gmail.com>
2022-01-18 08:08:51 +0330

    changes

---
+++ _snapshot/photos/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Dictionary from "./Dictionary";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary defaultKeyword="sunset" />
+        </main>
+      </div>
+    </div>
+  );
+}
